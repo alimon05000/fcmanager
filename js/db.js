412221124c6c5d540c3f0a4cbeacf567c726fc45
alimon05000 <alimsrifrv23@gmail.com>
2026-06-03 19:04:02 +0300

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,13 +1,22 @@
 const db = {
     clubs: [
-        { id: 'zen', name: 'Зенит', logo: 'img/zenit.png', colors: ['#00a3e6', '#ffffff'], budget: 95, ovr: 82 },
-        { id: 'spa', name: 'Спартак', logo: 'img/spartak.png', colors: ['#e30613', '#ffffff'], budget: 65, ovr: 79 },
-        { id: 'csk', name: 'ЦСКА', logo: 'img/cska.png', colors: ['#0033a0', '#e30613'], budget: 50, ovr: 78 },
-        { id: 'dyn', name: 'Динамо', logo: 'img/dynamo.png', colors: ['#005fa8', '#ffffff'], budget: 55, ovr: 79 },
-        { id: 'kra', name: 'Краснодар', logo: 'img/krasnodar.png', colors: ['#005e36', '#000000'], budget: 60, ovr: 80 },
-        { id: 'lok', name: 'Локомотив', logo: 'img/lokomotiv.png', colors: ['#008033', '#e30613'], budget: 45, ovr: 77 }
-        // Добавь остальные клубы РПЛ по аналогии
+        { id: 'zen', name: 'Зенит', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/FC_Zenit_1_star_2015.png/240px-FC_Zenit_1_star_2015.png', colors: ['#00a3e6', '#ffffff'], budget: 95, ovr: 82 },
+        { id: 'spa', name: 'Спартак', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/FC_Spartak_Moscow_logo.svg/200px-FC_Spartak_Moscow_logo.svg.png', colors: ['#e30613', '#ffffff'], budget: 65, ovr: 79 },
+        { id: 'csk', name: 'ЦСКА', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/PFC_CSKA_Moscow_logo.svg/200px-PFC_CSKA_Moscow_logo.svg.png', colors: ['#0033a0', '#e30613'], budget: 50, ovr: 78 },
+        { id: 'dyn', name: 'Динамо', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/FC_Dynamo_Moscow_logo.svg/200px-FC_Dynamo_Moscow_logo.svg.png', colors: ['#005fa8', '#ffffff'], budget: 55, ovr: 79 },
+        { id: 'kra', name: 'Краснодар', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/FC_Krasnodar_logo.svg/200px-FC_Krasnodar_logo.svg.png', colors: ['#005e36', '#000000'], budget: 60, ovr: 80 },
+        { id: 'lok', name: 'Локомотив', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/FC_Lokomotiv_Moscow_logo.svg/200px-FC_Lokomotiv_Moscow_logo.svg.png', colors: ['#008033', '#e30613'], budget: 45, ovr: 77 },
+        { id: 'ros', name: 'Ростов', logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/9/91/FC_Rostov_logo.svg/200px-FC_Rostov_logo.svg.png', colors: ['#fcd015', '#004f9e'], budget: 25, ovr: 74 },
+        { id: 'rub', name: 'Рубин', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/FC_Rubin_Kazan_logo.svg/200px-FC_Rubin_Kazan_logo.svg.png', colors: ['#990033', '#005fa8'], budget: 30, ovr: 74 },
+        { id: 'krs', name: 'Крылья Советов', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/FC_Krylia_Sovetov_logo.svg/200px-FC_Krylia_Sovetov_logo.svg.png', colors: ['#00a3e6', '#000000'], budget: 20, ovr: 73 },
+        { id: 'akh', name: 'Ахмат', logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/1/1d/FC_Akhmat_Grozny_logo.svg/200px-FC_Akhmat_Grozny_logo.svg.png', colors: ['#008033', '#ffffff'], budget: 18, ovr: 72 },
+        { id: 'fak', name: 'Факел', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/FC_Fakel_Voronezh_logo.svg/200px-FC_Fakel_Voronezh_logo.svg.png', colors: ['#0033a0', '#ffffff'], budget: 12, ovr: 69 },
+        { id: 'pnn', name: 'Пари НН', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/FC_Nizhny_Novgorod_logo.svg/200px-FC_Nizhny_Novgorod_logo.svg.png', colors: ['#00a3e6', '#000000'], budget: 15, ovr: 70 },
+        { id: 'ore', name: 'Оренбург', logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/0/05/FC_Orenburg_logo.svg/200px-FC_Orenburg_logo.svg.png', colors: ['#005fa8', '#ffffff'], budget: 14, ovr: 71 },
+        { id: 'makh', name: 'Динамо Мх', logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/8/87/FC_Dynamo_Makhachkala_logo.svg/200px-FC_Dynamo_Makhachkala_logo.svg.png', colors: ['#005fa8', '#ffffff'], budget: 10, ovr: 68 },
+        { id: 'khim', name: 'Химки', logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/d/d7/FC_Khimki_logo.svg/200px-FC_Khimki_logo.svg.png', colors: ['#e30613', '#000000'], budget: 13, ovr: 69 },
+        { id: 'akr', name: 'Акрон', logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/5/52/FC_Akron_Tolyatti_logo.svg/200px-FC_Akron_Tolyatti_logo.svg.png', colors: ['#e30613', '#000000'], budget: 12, ovr: 70 }
     ],
-    fNames: ["Иван", "Алексей", "Данил", "Артем", "Денис"],
-    lNames: ["Иванов", "Смирнов", "Попов", "Соколов", "Новиков"]
+    fNames: ["Иван","Алексей","Егор","Данил","Артем","Денис","Роман","Илья","Максим","Сергей"],
+    lNames: ["Иванов","Смирнов","Попов","Соколов","Новиков","Морозов","Волков","Зайцев","Лебедев","Козлов"]
 };
