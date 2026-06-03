1dc20417e967bc840765b444a0b6716263de9fb4
alimon05000 <alimsrifrv23@gmail.com>
2026-06-03 18:58:25 +0300

    Create db.js

---
+++ js/db.js
@@ -0,0 +1,13 @@
+const db = {
+    clubs: [
+        { id: 'zen', name: 'Зенит', logo: 'img/zenit.png', colors: ['#00a3e6', '#ffffff'], budget: 95, ovr: 82 },
+        { id: 'spa', name: 'Спартак', logo: 'img/spartak.png', colors: ['#e30613', '#ffffff'], budget: 65, ovr: 79 },
+        { id: 'csk', name: 'ЦСКА', logo: 'img/cska.png', colors: ['#0033a0', '#e30613'], budget: 50, ovr: 78 },
+        { id: 'dyn', name: 'Динамо', logo: 'img/dynamo.png', colors: ['#005fa8', '#ffffff'], budget: 55, ovr: 79 },
+        { id: 'kra', name: 'Краснодар', logo: 'img/krasnodar.png', colors: ['#005e36', '#000000'], budget: 60, ovr: 80 },
+        { id: 'lok', name: 'Локомотив', logo: 'img/lokomotiv.png', colors: ['#008033', '#e30613'], budget: 45, ovr: 77 }
+        // Добавь остальные клубы РПЛ по аналогии
+    ],
+    fNames: ["Иван", "Алексей", "Данил", "Артем", "Денис"],
+    lNames: ["Иванов", "Смирнов", "Попов", "Соколов", "Новиков"]
+};
